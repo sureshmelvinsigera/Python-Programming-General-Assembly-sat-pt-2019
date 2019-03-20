d705bff86ab5455967227c83d43a1024fc3cbce1
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-03-20 16:21:17 -0400

    Initial commit

---
+++ Unit1/ex4.py
@@ -0,0 +1,12 @@
+balance = 990 + 10  # addition
+print(balance)
+balance = balance - 10  # subtraction
+print(balance)
+balance = balance * 1.0  # multiplication
+print(balance)
+balance = balance / 10  # division
+print(balance)
+balance = balance % 5  # the remainder after dividing 99 by 5
+print(balance)
+balance = balance ** 2  # exponents
+print(balance)
