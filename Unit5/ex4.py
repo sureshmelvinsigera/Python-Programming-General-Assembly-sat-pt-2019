9525d16148bb11b6eac8cb890a95c5b93b82f51d
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-04-17 13:48:39 -0400

    changed file opening scope

--- Unit5/ex4.py
+++ Unit5/ex4.py
@@ -1,18 +1,23 @@
 def read_data(file_name):
+    file = open(file_name, 'r')
     data = {}
     counter = 1
     try:
-        file = open(file_name, 'r')
         for line in file:
             field = line.split(" ")
+            # read the data, and adding to the dictionary.
             data['invoice' + str(counter)] = field[0]
             data['items'] = field[1]
             data['price'] = field[2]
             counter += 1
+        # closing the file
         file.close()
+        # returning the data dictionary
         return data
     except FileNotFoundError:
         print(file_name, 'is not exists')
+    finally:
+        file.close()
 
 
 if __name__ == '__main__':
