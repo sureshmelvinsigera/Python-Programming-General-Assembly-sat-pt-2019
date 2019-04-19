0f820e9fa553f42d1d17d2103227f1a413ee41b4
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-04-19 15:39:18 -0400

    Initial commit

--- Unit5/ex7.py
+++ Unit5/ex7.py
@@ -1,25 +1,25 @@
 import datetime
 
-today = datetime.date.today()
-print(today)
-print(today.month)
-print(today.year)
-
-print('==================')
-now = datetime.datetime.now()
-print(now)
-
-# The strftime() Method
-
-time = datetime.datetime.now()
-
-print('Weekday, short version', time.strftime("%a"))
-print('Weekday, full version', time.strftime("%A"))
-print('Weekday as a number 0-6, 0 is Sunday', time.strftime("%w"))
-print('Month name, short version', time.strftime("%b"))
-print('Month name, full version', time.strftime("%B"))
-print('Month as a number 01-12', time.strftime("%m"))
-print('AM/PM', time.strftime("%p"))
-print('Year, short version, without century', time.strftime("%Y"))
-print('Year, full version', time.strftime("%y"))
-print('Year, full version', time.strftime("%y"))
+# today = datetime.date.today()
+# print(today)
+# print(today.month)
+# print(today.year)
+#
+# print('==================')
+# now = datetime.datetime.now()
+# print(now)
+#
+# # The strftime() Method
+#
+# time = datetime.datetime.now()
+#
+# print('Weekday, short version', time.strftime("%a"))
+# print('Weekday, full version', time.strftime("%A"))
+# print('Weekday as a number 0-6, 0 is Sunday', time.strftime("%w"))
+# print('Month name, short version', time.strftime("%b"))
+# print('Month name, full version', time.strftime("%B"))
+# print('Month as a number 01-12', time.strftime("%m"))
+# print('AM/PM', time.strftime("%p"))
+# print('Year, short version, without century', time.strftime("%Y"))
+# print('Year, full version', time.strftime("%y"))
+# print('Year, full version', time.strftime("%y"))
